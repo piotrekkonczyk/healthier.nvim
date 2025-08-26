c32717ea7c253185974f379881e66edd4ad2697b
piotrekkonczyk <konczyk.piotrek@gmail.com>
2025-08-26 10:15:03 +0200

    feat: works when require via lua

--- lua/healthier/init.lua
+++ lua/healthier/init.lua
@@ -1,13 +1,7 @@
 local M = {}
 
-M.hello_world = function()
-  error('this throws an error')
-end
-
 M.setup = function()
-  for _ in 10 do
-    print('Hello world!')
-  end
+  print('Hello world!')
 end
 
 return M
