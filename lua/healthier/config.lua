418a9303fda585f71ced303cc0864a087b297537
piotrekkonczyk <konczyk.piotrek@gmail.com>
2025-08-27 16:47:52 +0200

    feat: created minimal config for start screen

--- lua/healthier/config.lua
+++ lua/healthier/config.lua
@@ -1,5 +1,13 @@
+---@class StartScreenConfig
+---@field show_by_default boolean
+local start_screen_config = {
+  show_by_default = true,
+}
+
+---@class Config
+---@field start_screen_config StartScreenConfig
 local config = {
-  show_start_screen = true,
+  start_screen_config = start_screen_config,
 }
 
 return config
