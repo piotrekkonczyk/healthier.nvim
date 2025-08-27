aa20fd76856398291be306ef1b6877be1e308095
piotrekkonczyk <konczyk.piotrek@gmail.com>
2025-08-27 16:50:30 +0200

    chore: setup function can take nil as well

--- lua/healthier/init.lua
+++ lua/healthier/init.lua
@@ -2,10 +2,11 @@ local start_screen = require('healthier.ui.start_screen')
 local config = require('healthier.config')
 local M = {}
 
+---@param opts Config | nil
 M.setup = function(opts)
   opts = opts or config
 
-  if opts.show_start_screen then
+  if opts.start_screen_config.show_by_default then
     start_screen.show_start_screen()
   end
 end
